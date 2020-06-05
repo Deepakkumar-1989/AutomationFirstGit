c88943f10942ff4aa59fff33ec41e394cdaf9dc0
Deepakkumar-1989 <deepakkumar.globus01@gmail.com>
2020-06-05 18:20:27 +0530

    Test commit

--- src/test/java/com/ComName/ProName/testCase/TC004_Create_an_Account.java
+++ src/test/java/com/ComName/ProName/testCase/TC004_Create_an_Account.java
@@ -5,7 +5,8 @@ import java.util.Hashtable;
 import org.testng.SkipException;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import com.ComName.ProName.Utility.CommonMethods;
+import com.ComName.ProName.pages.HomePage;
+import com.ComName.ProName.pages.YourPersonalInformationPage;
 import com.ComName.ProName.testBase.TestBase;
 
 public class TC004_Create_an_Account extends TestBase{
@@ -22,23 +23,13 @@ public class TC004_Create_an_Account extends TestBase{
 
 public static void TC004_Create_An_Account(Hashtable <String, String>  testdata) throws IOException, InterruptedException {
 	
-	launchBrowser();
-	
-    CommonMethods.clickOnWebelement("XSignInBtn", "Sign In Button");
-	
-	CommonMethods.writeIntoInputBox("XEmail_add", testdata, "Email_Id");
-	
-	CommonMethods.clickOnWebelement("XCreatAcnt_btn", "Create An Account Button");
-			
-	CommonMethods.clickOnWebelement("XGender", "Mr Title has been selected");
-	
-	CommonMethods.writeIntoInputBox("XFirstName", testdata, "First_Name");
-			
-	CommonMethods.writeIntoInputBox("XLastName", testdata, "Last_Name");
-	
-	CommonMethods.writeIntoInputBox("Xpwd", testdata, "Password");
-	
-	closeBrowser();
+	//launchBrowser();
+
+			HomePage.CreateAnAccount(testdata);
+
+			YourPersonalInformationPage.enterYourPersonalInformation(testdata);
+
+			//closeBrowser();
 	
 }
 
